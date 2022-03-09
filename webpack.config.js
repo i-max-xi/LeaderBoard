ba8bb14db7395d221be4de43ccfff3604aa070e1
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-03-09 23:02:37 +0000

    setup template

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      template: "./src/index.html",
     }),
   ],
   output: {
