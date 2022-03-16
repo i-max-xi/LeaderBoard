fb01bbbab519e92c7d022a7c6b9589ec002c4cb8
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-03-16 02:37:27 +0000

    Add form reset

--- modules/add.js
+++ modules/add.js
@@ -3,13 +3,13 @@ import { postData, getData } from './api.js';
 const addPoints = () => {
   const name = document.querySelector('#name');
   const point = document.querySelector('#score');
-  // const form = document.querySelector('.form');
+  const form = document.querySelector('#myform');
   const raw = JSON.stringify({
     user: name.value,
     score: point.value,
   });
   postData(raw);
-  // form.reset();
+  form.reset();
   getData();
 };
 
